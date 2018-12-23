397df17cb2f5956751136f33263d6393bbd08988
Yu-Song Syu <kuma@simbo.com.tw>
2018-12-23 11:11:54 +0800

    no deuce case ok

--- src/main/java/idv/kuma/TennisGame.java
+++ src/main/java/idv/kuma/TennisGame.java
@@ -7,11 +7,29 @@ public class TennisGame {
 
 
     public TennisGame a() {
-        aPoint += 15;
+        aPoint = getNext(aPoint);
         return this;
     }
 
+
+    public TennisGame b() {
+        bPoint = getNext(bPoint);
+        return this;
+    }
+
+
+    private int getNext(int current) {
+        if (current == 30) {
+            return 40;
+        } else {
+            return current + 15;
+        }
+    }
+
     public String getResult() {
+        if (aPoint == 55) return "A Win";
+        if (bPoint == 55) return "B Win";
         return aPoint + ":" + bPoint;
     }
 }
+
--- src/test/java/idv/kuma/TennisGameTest.java
+++ src/test/java/idv/kuma/TennisGameTest.java
@@ -1,12 +1,9 @@
 package idv.kuma;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest {
-
 
 
     @Test
@@ -14,8 +11,17 @@ public class TennisGameTest {
         TennisGame game = createGame();
 
         game.a();
-        
+
         Assert.assertEquals("15:0", game.getResult());
+    }
+
+    @Test
+    public void When_B_Then_0_15() throws Exception {
+        TennisGame game = createGame();
+
+        game.b();
+
+        Assert.assertEquals("0:15", game.getResult());
     }
 
     private TennisGame createGame() {
@@ -29,6 +35,51 @@ public class TennisGameTest {
         game.a().a();
 
         Assert.assertEquals("30:0", game.getResult());
+    }
+    @Test
+    public void When_BB_Then_0_30() throws Exception {
+        TennisGame game = createGame();
+
+        game.b().b();
+
+        Assert.assertEquals("0:30", game.getResult());
+    }
+
+    @Test
+    public void When_AAA_Then_40_0() throws Exception {
+        TennisGame game = createGame();
+
+        game.a().a().a();
+
+        Assert.assertEquals("40:0", game.getResult());
+    }
+
+    @Test
+    public void When_BBB_Then_0_40() throws Exception {
+        TennisGame game = createGame();
+
+        game.b().b().b();
+
+        Assert.assertEquals("0:40", game.getResult());
+    }
+
+    @Test
+    public void When_AAAA_Then_A_WIN() throws Exception {
+        TennisGame game = createGame();
+
+        game.a().a().a().a();
+
+        Assert.assertEquals("A Win", game.getResult());
+    }
+
+
+    @Test
+    public void When_BBBB_Then_B_WIN() throws Exception {
+        TennisGame game = createGame();
+
+        game.b().b().b().b();
+
+        Assert.assertEquals("B Win", game.getResult());
     }
 
 
