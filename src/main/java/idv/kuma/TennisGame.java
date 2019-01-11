a0af0a5f3c6c1d1748a9963cd360c9c27803a150
Yu-Song Syu <kuma@simbo.com.tw>
2019-01-11 17:50:16 +0800

    back to nothing

--- src/main/java/idv/kuma/TennisGame.java
+++ src/main/java/idv/kuma/TennisGame.java
@@ -1,39 +1,6 @@
 package idv.kuma;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TennisGame {
 
 
-    private int aHits = 0;
-    private int bHits = 0;
-
-    private Map<Integer, Integer> hitsToScore = new HashMap<>();
-
-
-    {
-        hitsToScore.put(0, 0);
-        hitsToScore.put(1, 15);
-        hitsToScore.put(2, 30);
-        hitsToScore.put(3, 40);
-    }
-
-    public TennisGame a() {
-        aHits++;
-        return this;
-    }
-
-    public TennisGame b() {
-        bHits++;
-        return this;
-    }
-
-    public String show() {
-
-        if (aHits == 4) return "A Wins";
-        if (bHits == 4) return "B Wins";
-
-        return hitsToScore.get(aHits) + ":" + hitsToScore.get(bHits);
-    }
 }
--- src/test/java/idv/kuma/TennisGameTest.java
+++ src/test/java/idv/kuma/TennisGameTest.java
@@ -1,64 +1,6 @@
 package idv.kuma;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 public class TennisGameTest {
-
-
-    private TennisGame game;
-
-    @Before
-    public void setUp() throws Exception {
-        this.game = new TennisGame();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void When_A_Then_15_0() throws Exception {
-
-
-
-        game.a();
-
-        Assert.assertEquals("15:0", game.show());
-
-    }
-
-    @Test
-    public void When_AA_Then_30_0() throws Exception {
-
-
-        game.a().a();
-
-        Assert.assertEquals("30:0", game.show());
-
-    }
-
-    @Test
-    public void When_AAA_Then_40_0() throws Exception {
-
-
-        game.a().a().a();
-
-        Assert.assertEquals("40:0", game.show());
-
-    }
-
-    @Test
-    public void When_AAAA_Then_A_WINS() throws Exception {
-
-
-        game.a().a().a().a();
-
-        Assert.assertEquals("A Wins", game.show());
-
-    }
 
 
 }
