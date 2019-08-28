dcac48523d611e8ac0895044e02ed420395fcdd8
Yu-Song Syu <kuma@simbo.com.tw>
2019-08-29 07:23:29 +0800

    bad example

--- kuma/salary/src/main/java/idv/kuma/SalaryProfile.java
+++ kuma/salary/src/main/java/idv/kuma/SalaryProfile.java
@@ -1,8 +1,13 @@
 package idv.kuma;
 
+import java.time.Year;
+import java.time.YearMonth;
+
 public class SalaryProfile {
 
     String name;
+
+    YearMonth arriveYearMonth;
 
     double monthlySalary;
 
--- kuma/salary/src/main/java/idv/kuma/SalaryService.java
+++ kuma/salary/src/main/java/idv/kuma/SalaryService.java
@@ -1,16 +1,38 @@
 package idv.kuma;
 
+import java.time.YearMonth;
+import java.util.List;
+
 /**
  * Hello world!
  */
 public class SalaryService {
 
 
+    private SalaryProfileRepo salaryProfileRepo;
+
+    public SalaryService(SalaryProfileRepo salaryProfileRepo) {
+        this.salaryProfileRepo = salaryProfileRepo;
+    }
+
     public double getAll() {
 
-        
+        List<SalaryProfile> profiles = salaryProfileRepo.getAll();
 
-        return 0.0;
+
+        double total = 0.0;
+        for (SalaryProfile profile : profiles) {
+
+
+            if (profile.annualSalary > 0) {
+                total += profile.annualSalary;
+            } else {
+                total += profile.monthlySalary * 12;
+            }
+
+        }
+
+        return total;
 
     }
 
