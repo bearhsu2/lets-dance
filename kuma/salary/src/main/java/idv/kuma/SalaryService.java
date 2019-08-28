49037d87b98bca17a837ed3a8835bd6ffbe07d89
Yu-Song Syu <kuma@simbo.com.tw>
2019-08-29 07:08:26 +0800

    init version

---
+++ kuma/salary/src/main/java/idv/kuma/SalaryService.java
@@ -0,0 +1,13 @@
+package idv.kuma;
+
+/**
+ * Hello world!
+ *
+ */
+public class SalaryService
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
