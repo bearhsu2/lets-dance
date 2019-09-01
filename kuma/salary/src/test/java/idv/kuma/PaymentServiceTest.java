dcac41d3bff2ca40923481c5c25d93a0351a77cd
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-01 20:42:29 +0800

    refactor test: extract method prepareEmployeeRepo

--- kuma/salary/src/test/java/idv/kuma/PaymentServiceTest.java
+++ kuma/salary/src/test/java/idv/kuma/PaymentServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 
@@ -17,15 +18,23 @@ public class PaymentServiceTest {
     @Test
     public void When_Annual_Then_Direct_AnnualPay() {
 
-        EmployeeRepo mockedEmployeeRepo = Mockito.mock(EmployeeRepo.class);
-        when(mockedEmployeeRepo.getAll()).thenReturn(Arrays.asList(
+        EmployeeRepo mockedEmployeeRepo = prepareEmployeeRepo(
                 new Employee(300)
-        ));
+        );
 
 
         PaymentService paymentService = new PaymentService(mockedEmployeeRepo);
 
         Assert.assertEquals(300, paymentService.getAll(), 0.001);
+    }
+
+    private EmployeeRepo prepareEmployeeRepo(Employee... employees) {
+        EmployeeRepo mockedEmployeeRepo = Mockito.mock(EmployeeRepo.class);
+
+        when(mockedEmployeeRepo.getAll()).thenReturn(
+                Arrays.asList(employees)
+        );
+        return mockedEmployeeRepo;
     }
 
 }
